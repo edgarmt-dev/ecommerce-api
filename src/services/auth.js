8cf1aeca0bfffe104156b8b2e40d9bbb4e0f5a3a
EdgarMTdev <edgarmontiel961@gmail.com>
2022-05-27 12:14:09 -0500

    add: encrypt password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const bcrypt = require('bcrypt')
 
 class Auth {
 
@@ -6,8 +7,16 @@ class Auth {
         this.userService = new User()
     }
 
+    async #encrypt(password) {
+        const salt = await bcrypt.genSalt(10)
+        return await bcrypt.hash(password, salt)
+    }
+
     async create(data) {
         try {
+            if(data && data.password) {
+                data.password = await this.#encrypt(data.password)
+            }
             const result = await this.userService.create(data)
             return result
         } catch (error) {
